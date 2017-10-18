34d2beb3e6d99ae731f1b59e74a15e95f7f72ec7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-10-18 21:01:08 -0200

    Criando a classe 'Produto' usando o mongoose.

---
+++ app/models/produto.js
@@ -0,0 +1,28 @@
+/**
+ * Arquivo: produto.js
+ * Author: Glaucia Lemos
+ * Descrição: arquivo responsável onde trataremos o modelo da classe 'Produto'
+ * Data: 18/10/2017
+ * obs.: http://mongoosejs.com/docs/schematypes.html
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+/**
+ * Produto:
+ * 
+ * -> Id: int
+ * -> Nome: String
+ * -> Preco: Number
+ * -> Descricao: String
+ * 
+ */
+
+var ProdutoSchema = new Schema({
+    nome: String,
+    preco: Number,
+    descricao: String
+});
+
+module.exports = mongoose.model('Produto', ProdutoSchema);
